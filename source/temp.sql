9976cbb1bfd156e056fb8e4fb0a12b287d0a7b9b
Peter Ocelka <peter.ocelka@gmail.com>
2022-11-14 21:58:34 +0100

    chore: backup work

--- source/temp.sql
+++ source/temp.sql
@@ -11,3 +11,13 @@ from wms_log
 where 1 = 1
 order by 1 desc
 ;
+
+
+
+condition: "EXT_CUSTOMER" COMPLETED
+action: START "EXT_CONTACT"
+
+
+
+condition: "EXT_ADDRESS" SUCCEEDED and "EXT_SR" SUCCEEDED and "EXT_CONTACT" SUCCEEDED
+action: START "EXT_ASSET"
